356e5ea8c73f7fe78ace8eddcb4a09ab17b4641a
EricRiney <fails.to.install@gmail.com>
2017-08-13 12:03:04 -0700

    I hate

--- hello/hello.go
+++ hello/hello.go
@@ -1,13 +1,25 @@
-
 package main
 
-import (
-	"fmt"
-	"time"
-	"github.com/newUser/stringutil"
-)
+import "net"
+import "fmt"
+import "bufio"
+//import "os"
 
 func main() {
-	fmt.Println("The time is ", time.Now())
-	fmt.Println(stringutil.Reverse("!tes   t oG ,olleH"))
+
+  // connect to this socket
+  // conn, _ := net.Dial("tcp", "127.0.0.1:9090")
+  for { 
+	conn, _ := net.Dial("tcp", "127.0.0.1:9090")
+    // read in input from stdin
+    // reader := bufio.NewReader(os.Stdin)
+    fmt.Print("Text to send: ")
+	text := "" 
+	//, _ := reader.ReadString('\n')
+    // send to socket
+    fmt.Fprintf(conn, text + "\n")
+    // listen for reply
+    message, _ := bufio.NewReader(conn).ReadString('\n')
+    fmt.Print("Message from server: "+message)
+  }
 }
