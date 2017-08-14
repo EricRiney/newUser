34249e8d7febcc63f3ea7387b7046c00f874ab03
EricRiney <fails.to.install@gmail.com>
2017-08-13 18:42:51 -0700

    fin

--- hello/hello.go
+++ hello/hello.go
@@ -1,27 +1,11 @@
 package main
 
-import "net"
-import "fmt"
-//import "bufio"
-//import "os"
+import (
+	"fmt"
+
+	"github.com/golang/example/stringutil"
+)
 
 func main() {
-
-  // connect to this socket
-  // conn, _ := net.Dial("tcp", "127.0.0.1:9090")
-  for { 
-	conn, _ := net.Dial("tcp", "127.0.0.1:9090")
-    // read in input from stdin
-    // reader := bufio.NewReader(os.Stdin)
-    // fmt.Print("Text to send: ")
-	var text = "" 
-	//http.ListenAndServe(":8080", nil)
-	//, _ := reader.ReadString('\n')
-    // send to socket
-    fmt.Fprintf(conn, text)
-    // listen for reply
-	var message = "abc"
-	//, _ := bufio.NewReader(conn).ReadString('\n')
-    fmt.Print("Message from server: "+message)
-  }
+	fmt.Println(stringutil.Reverse("!selpmaxe oG ,olleH"))
 }
